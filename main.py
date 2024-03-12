4e3c7c9dca3b0936f705c54e823f78df2e9dd0b2
Baillora <115589829+Baillora@users.noreply.github.com>
2024-03-13 01:03:30 +0700

    Update main.py
    bugfixes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+import shutil
+import random
+import string
 def find_unique_lines():
     script_directory = os.path.dirname(os.path.abspath(__file__))
     data_directory = os.path.join(script_directory, 'data')
@@ -8,9 +11,25 @@ def find_unique_lines():
         if os.path.isfile(filepath) and filename.endswith('.txt'):
             with open(filepath, 'r', encoding='utf-8') as file:
                 unique_lines.update(line.strip() for line in file)
-    output_file_path = os.path.join(script_directory, 'unique_lines.txt')
+    random_name = ''.join(random.choices(string.ascii_letters + string.digits, k=15))
+    output_file_path = os.path.join(script_directory, f'unique_lines_{random_name}.txt')
+    while os.path.exists(output_file_path):
+        random_name = ''.join(random.choices(string.ascii_letters + string.digits, k=15))
+        output_file_path = os.path.join(script_directory, f'unique_lines_{random_name}.txt')
     with open(output_file_path, 'w', encoding='utf-8') as output_file:
         for line in unique_lines:
             output_file.write(line + '\n')
     print(f"Сохраннёный результат by Baillora: {output_file_path}")
-find_unique_lines()
+    return output_file_path
+def process_data_folder():
+    output_file_path = find_unique_lines()
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_directory = os.path.join(script_directory, 'data')
+    for filename in os.listdir(data_directory):
+        filepath = os.path.join(data_directory, filename)
+        if os.path.isfile(filepath):
+            os.remove(filepath)
+        elif os.path.isdir(filepath):
+            shutil.rmtree(filepath)
+    print("Файлы в папке 'data' успешно удалены.")
+process_data_folder()
